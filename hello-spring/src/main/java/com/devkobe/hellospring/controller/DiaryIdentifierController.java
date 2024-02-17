a39a1b24706e1ef4c6b27806c4eb9b07e1760e00
Kobe <dev.skyachieve91@gmail.com>
2024-02-17 13:35:54 +0900

    :sparkles: Feat: 'MVC와 템플릿 엔진' 실습.

--- hello-spring/src/main/java/com/devkobe/hellospring/controller/DiaryIdentifierController.java
+++ hello-spring/src/main/java/com/devkobe/hellospring/controller/DiaryIdentifierController.java
@@ -3,6 +3,7 @@ package com.devkobe.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DiaryIdentifierController {
@@ -10,5 +11,11 @@ public class DiaryIdentifierController {
     public String diaryId(Model model) {
         model.addAttribute("diaryIdData", "1");
         return "diaryId";
+    }
+
+    @GetMapping("diary")
+    public String hello(@RequestParam("userName") String userName, Model model) {
+        model.addAttribute("userName", userName);
+        return "getAllDiaryTemplate";
     }
 }
